464d8073a0a10ddd3881b03bf117a3d71da3fa79
Elvis <egannem@gmail.com>
2022-09-19 14:15:47 -0300

    Migration: criação de tabela de produtos

---
+++ database/migrations/2022_09_19_171422_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('codigo_produto');
+            $table->string('nome');
+            $table->string('cor');
+            $table->string('tamanho');
+            $table->integer('valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
