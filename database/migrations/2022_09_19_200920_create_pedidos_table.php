15bc4dcb1116efca9d7884949025dd50109b2ff2
Elvis <egannem@gmail.com>
2022-09-19 17:14:19 -0300

    Migration: criação de tabela de pedidos

---
+++ database/migrations/2022_09_19_200920_create_pedidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->uuid('codigo_pedido');
+            $table->date('data_pedido');
+            $table->text('observacao')->nullable();
+            $table->string('forma_pagamento');
+            $table->json('obj_pedidos');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('clientes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
