f2f7cdc14490c9fba5249addfac09b0bf0e68e42
Elvis <egannem@gmail.com>
2022-09-20 14:29:50 -0300

    Criação de navbar - front

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <h2>CadastroApp</h2>
+        <ul>
+            <li>Clientes</li>
+            <li>Produtos</li>
+            <li>Pedidos</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 4rem;
+    background: #e2e8f0;
+}
+
+ul {
+    display: flex;
+    gap: 1rem;
+    list-style-type: none;
+}
+
+</style>
